663fed7d955aee3bd136ea570cff1704e3ff4851
kato <kato at win8>
2013-12-01 16:08:44 +0900

    テスト結果NGとなったテストの追加

---
+++ src/UnitTest1/BugFixTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class BugFixTest
+    {
+        /// <summary>
+        /// https://github.com/kkato233/simplehtmlparser/issues/2
+        /// &lt;body&gt タグを探そうとしたら見つからなかった バグの修正
+        /// </summary>
+        [TestMethod]
+        public void TestFix02()
+        {
+            // HTML パーサーインスタンス
+            HtmlTools.HtmlParser p = new HtmlTools.HtmlParser();
+
+            // 入力のHTML
+            string html =
+@"<html>
+<head>
+</head>
+<body>
+hello world.
+<div class='test'>div-test<br><unknwontag></div>
+<br>
+</body>
+</html>";
+
+            // 文字列を解析してDOMにする
+            var dom = p.ParseHtmlString(html);
+
+            var body = dom.GetElementsByTagName("body");
+
+            Assert.IsTrue(body != null);
+            Assert.AreEqual(1, body.Count());
+        }
+
+        [TestMethod]
+        public void TestFix03()
+        {
+            // https://github.com/kkato233/simplehtmlparser/issues/3
+            // 属性を書き換えたら ' が " に代わってしまった
+
+            // HTML パーサーインスタンス
+            HtmlTools.HtmlParser p = new HtmlTools.HtmlParser();
+
+            // 入力のHTML
+            string html =
+@"<html>
+<head>
+</head>
+<body>
+<div class='test'></div>
+<br>
+</body>
+</html>";
+
+            // 文字列を解析してDOMにする
+            var dom = p.ParseHtmlString(html);
+
+            var div = dom.GetElementsByTagName("div").FirstOrDefault();
+
+            Assert.IsNotNull(div);
+            div.SetAttributes("class", "test2");
+
+            string outHtml = dom.ToHtmlString();
+            Assert.IsTrue(outHtml.Contains("<div class='test2'>"),outHtml);
+        }
+
+
+        [TestMethod]
+        public void TestFix03_A()
+        {
+            // https://github.com/kkato233/simplehtmlparser/issues/3
+            // 属性を書き換えたら ' が " に代わってしまった
+
+            // HTML パーサーインスタンス
+            HtmlTools.HtmlParser p = new HtmlTools.HtmlParser();
+
+            // 入力のHTML
+            string html = "<div class='te\"st'></div>";
+
+            // 文字列を解析してDOMにする
+            var dom = p.ParseHtmlString(html);
+
+            var div = dom.GetElementsByTagName("div").FirstOrDefault();
+
+            Assert.IsNotNull(div);
+            div.SetAttributes("class", "let's");
+
+            string outHtml = dom.ToHtmlString();
+            Assert.IsTrue(outHtml.Contains("<div class='let's'>"), outHtml);
+        }
+
+
+        [TestMethod]
+        public void TestFix03_B()
+        {
+            // https://github.com/kkato233/simplehtmlparser/issues/3
+            // 属性を書き換えたら ' が " に代わってしまった
+
+            // HTML パーサーインスタンス
+            HtmlTools.HtmlParser p = new HtmlTools.HtmlParser();
+
+            // 入力のHTML
+            string html = "<div class='\"'></div>";
+
+            // 文字列を解析してDOMにする
+            var dom = p.ParseHtmlString(html);
+
+            var div = dom.GetElementsByTagName("div").FirstOrDefault();
+
+            Assert.IsNotNull(div);
+            div.SetAttributes("class", "\"");
+
+            string outHtml = dom.ToHtmlString();
+            Assert.IsTrue(outHtml.Contains("<div class='\"'>"), outHtml);
+        }
+    }
+}
---
+++ src/UnitTest1/SimpleTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class SimpleTest
+    {
+        /// <summary>
+        /// 使い方を簡単に説明するためのテストプログラム
+        /// </summary>
+        [TestMethod]
+        public void Test01_SimpleTest()
+        {
+            // HTML パーサーインスタンス
+            HtmlTools.HtmlParser p = new HtmlTools.HtmlParser();
+
+            // 入力のHTML
+            string html = 
+@"<html>
+<head>
+</head>
+<body>
+hello world.
+<div class='test'>div-test<br><unknwontag></div>
+<br>
+</body>
+</html>";
+
+            // 文字列を解析してDOMにする
+            var dom = p.ParseHtmlString(html);
+
+            Assert.IsNotNull(dom);
+
+            // DIV タグを探す。大文字小文字は関係なく探す
+            var divList = dom.GetElementsByTagName("Div");
+            Assert.IsNotNull(divList);
+            Assert.AreEqual(1, divList.Count());
+
+            // div タグを選択
+            var div = divList.First();
+            
+            // div タグの InnerHtml を書き換え
+            div.InnerHtml = "";
+
+            // DOM の HTML を取得する
+            string html2 = dom.ToHtmlString();
+
+            // DIV の内容が書き換わっている事の確認
+            Assert.IsTrue(html2.Contains("<div class='test'></div>"));
+        }
+    }
+}
